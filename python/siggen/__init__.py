dc9a011700b79de7cb29351f3d61ddaa5cd028d3
Ben Shanks <benjamin.shanks@gmail.com>
2017-10-30 09:18:43 -0400

    dont require scipy

--- python/siggen/__init__.py
+++ python/siggen/__init__.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.1"
-
 try:
     __SIGGEN_SETUP__
 except NameError:
     __SIGGEN_SETUP__ = False
 
 if not __SIGGEN_SETUP__:
-    __all__ = ["Detector", "ICPC"]
+    __all__ = ["Detector"]
 
     # from ._siggen import PyICPC,PyGEM, PyPPC
     # from .PyDetector import PyDetector
